8044890a919fc24cf18bec8603e036e6495e4cc9
duamemon51 <duamemon51@gmail.com>
2026-09-26 12:18:53 +0500

    ui

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -22,8 +22,8 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const STORE_ADDRESS = "RC7C+294 Shakeel General Store, Misri St, Hala, Pakistan";
-const STORE_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3591.7369849316724!2d68.41782047485763!3d25.81224980651555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x394b974047f2da73%3A0xfa231510cbfb77ea!2sRuman%20Mart!5e0!3m2!1sen!2s!4v1790365041555!5m2!1sen!2s";
-const STORE_MAP_URL = "https://www.google.com/maps/dir//Ruman+Mart,+Tariq+Rd,+near+Shareef+Decoration,+Hala,+70120,+Pakistan/@25.798964,68.4173972,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x394b974047f2da73:0xfa231510cbfb77ea!2m2!1d68.4203954!2d25.812245?entry=ttu&g_ep=EgoyMDI2MDkyMi4wIKXMDSoASAFQAw%3D%3D";
+const STORE_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57468.068251773286!2d68.34310680627823!3d25.8116798706532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x394b974047f2da73%3A0xfa231510cbfb77ea!2sRuman%20Mart!5e0!3m2!1sen!2s!4v1790406940826!5m2!1sen!2s";
+const STORE_MAP_URL = "https://www.google.com/maps/dir//Ruman+Mart,+shakeel+shopping+centre,+Tariq+Rd,+Hala,+70120,+Pakistan/@25.81015,68.422881,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x394b974047f2da73:0xfa231510cbfb77ea!2m2!1d68.4193253!2d25.8116797!3e0?hl=en&entry=ttu&g_ep=EgoyMDI2MDkyMy4wIKXMDSoASAFQAw%3D%3D";
 
 const contactInfo: {
   Icon: LucideIcon;
